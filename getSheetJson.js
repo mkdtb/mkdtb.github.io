0d7160de8c36c0540aa571b950bb909b2bae9669
mkdtb <53255144+mkdtb@users.noreply.github.com>
2019-08-24 20:36:58 +0900

    サーバーにリクエストする時にプレイヤー情報のみを取得する

--- getSheetJson.js
+++ getSheetJson.js
@@ -6,31 +6,30 @@ window.onload = function()
 
 function main()
 {
-    setInterval("getJsonp_GAS()", 1000);
+    setInterval("getJsonp_GAS()", 3000);
 }
  
 function getJsonp_GAS()
 {
     $.ajax({
         type: 'GET',
-        url: 'https://script.google.com/macros/s/AKfycbxK4VCS7w0VxCg5nv5CTOLvO5sbUCKuJdixiu1iA2Pv_2DXyH-o/exec',
+        url: 'https://script.google.com/macros/s/AKfycbxK4VCS7w0VxCg5nv5CTOLvO5sbUCKuJdixiu1iA2Pv_2DXyH-o/exec?param=PlayerData',
         dataType: 'jsonp',
         jsonpCallback: 'jsondata',
         success: function(json) {
-            var player1_name = json[0].player_name;
-            var player1_fontSize = json[0].fontSize;
-            var player1_fontColor = json[0].fontColor;
-            var player2_name = json[1].player_name;
-            var player2_fontSize = json[1].fontSize;
-            var player2_fontColor = json[1].fontColor;
+            var player_index;
             if (selectPlayer == "1p")
             {
-                insertHTML(player1_name, player1_fontColor, player1_fontSize);
+                player_index = 0;
             } else if (selectPlayer == "2p") {
-                insertHTML(player2_name, player2_fontColor, player2_fontSize);
+                player_index = 1;
             } else {
-                insertHTML("", 0 , 0);
+                return;
             }
+            var player_name = json[player_index].player_name;
+            var player_fontSize = json[player_index].fontSize;
+            var player_fontColor = json[player_index].fontColor;
+            insertHTML(player_name, player_fontColor, player_fontSize);
         }
     });
 }
